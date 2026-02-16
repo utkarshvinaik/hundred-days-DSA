8f0160687edd1adba896abb98168b874c79ac50d
UTKARSH VINAIK <vinaik.utkarsh2007@gmail.com>
2026-02-16 14:33:36 +0530

    Add files via upload

---
+++ DAY6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n <= 0) return 0;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("%d ", arr[0]);
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[i - 1]) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
+}
