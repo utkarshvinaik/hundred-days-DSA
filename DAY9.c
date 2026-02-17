0214f41e3b398eb0ccc307ad9679faa3de4e6e9f
UTKARSH VINAIK <vinaik.utkarsh2007@gmail.com>
2026-02-17 08:37:27 +0530

    Add files via upload

---
+++ DAY9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[1000];
+    
+    scanf("%s", str);
+
+    int length = strlen(str);
+
+    for(int i = length - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
